5887a0c890026088be3bcfc6da967eb1ac98361e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 02:00:36 +0300

    added a lifespan that gets rid of the db connection

--- main.py
+++ main.py
@@ -1,8 +1,18 @@
+from contextlib import asynccontextmanager
+
 from fastapi import FastAPI
 
 import uvicorn
 
-app = FastAPI()
+from utils.db_helper import db_helper
+
+
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    yield
+    await db_helper.engine.dispose()
+
+app = FastAPI(lifespan=lifespan)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
