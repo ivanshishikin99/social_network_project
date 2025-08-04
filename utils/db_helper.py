47122f7114d87c9ccc41f2f7e3109391ee591d1f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 01:59:33 +0300

    created a db_helper that allows for db connection disposing and session getting

---
+++ utils/db_helper.py
@@ -0,0 +1,36 @@
+from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker
+
+from core.config import settings
+
+
+class DbHelper:
+    def __init__(self,
+                 url: str,
+                 echo: bool,
+                 echo_pool: bool,
+                 max_overflow: int,
+                 pool_size: int):
+        self.engine = create_async_engine(url=url,
+                                          echo=echo,
+                                          echo_pool=echo_pool,
+                                          max_overflow=max_overflow,
+                                          pool_size=pool_size)
+
+        self.session_maker = async_sessionmaker(bind=self.engine,
+                                                autoflush=False,
+                                                expire_on_commit=False,
+                                                autocommit=False)
+
+    async def dispose(self):
+        await self.engine.dispose()
+
+    async def session_getter(self):
+        async with self.session_maker() as session:
+            yield session
+
+
+db_helper = DbHelper(url=str(settings.db_config.url),
+                     echo=settings.db_config.echo,
+                     echo_pool=settings.db_config.echo_pool,
+                     max_overflow=settings.db_config.max_overflow,
+                     pool_size=settings.db_config.pool_size)
