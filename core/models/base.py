0a018d1d56e875f9ba7786d7a123ba4c916f36a0
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 02:02:53 +0300

    created the base model

---
+++ core/models/base.py
@@ -0,0 +1,16 @@
+from sqlalchemy import MetaData
+from sqlalchemy.orm import DeclarativeBase, declared_attr
+
+from core.config import settings
+
+from utils import camel_case_to_snake_case
+
+
+class Base(DeclarativeBase):
+    metadata = MetaData(naming_convention=settings.db_config.naming_conventions)
+
+    __abstract__ = True
+
+    @declared_attr.directive
+    def __tablename__(cls):
+        return camel_case_to_snake_case(cls.__name__)
