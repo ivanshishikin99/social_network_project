4359d2e412f3cad90de3ce0c5143a92388d0a327
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 17:14:05 +0300

    added a register user endpoint

--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -1,3 +1,12 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api_v1.users.crud import create_user
+from api_v1.users.schemas import UserCreate
+from utils.db_helper import db_helper
 
 router = APIRouter(prefix='/users')
+
+@router.post('/create_user')
+async def register_user_view(user_data: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)):
+    return await create_user(user_data=user_data, session=session)
