63a5f636521ef439df12ecbd690f265f18f05ace
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 17:13:54 +0300

    user_id is now sent to the profile schema after creating and commiting the user schema

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -2,15 +2,17 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import UserCreate
 from core.models import User, Profile
+from utils.password_helpers import hash_password
 
 
 async def create_user(user_data: UserCreate, session: AsyncSession) -> User:
+    user_data.password = hash_password(password=user_data.password)
     user = User(**user_data.model_dump())
-    profile = Profile()
-    profile.user_id = user.id
     session.add(user)
-    session.add(profile)
     await session.commit()
     await session.refresh(user)
+    profile = Profile(user_id=user.id, is_public=True)
+    session.add(profile)
+    await session.commit()
     await session.refresh(profile)
     return user
