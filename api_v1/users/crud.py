6dc55e66ad3c78400d1a57ef52fa2939160c11cd
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 16:58:09 +0300

    the create user function now automatically created a profile connected to the user

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -1,12 +1,16 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from api_v1.users.schemas import UserCreate
-from core.models import User
+from core.models import User, Profile
 
 
 async def create_user(user_data: UserCreate, session: AsyncSession) -> User:
     user = User(**user_data.model_dump())
+    profile = Profile()
+    profile.user_id = user.id
     session.add(user)
+    session.add(profile)
     await session.commit()
     await session.refresh(user)
+    await session.refresh(profile)
     return user
