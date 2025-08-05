fd3995b78fd5fa6c756d87119d52d50334dfbd56
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-05 16:48:13 +0300

    created a users router

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ import uvicorn
 
 from utils.db_helper import db_helper
 
+from api_v1 import router as api_v1_router
+
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -13,6 +15,8 @@ async def lifespan(app: FastAPI):
     await db_helper.engine.dispose()
 
 app = FastAPI(lifespan=lifespan)
+
+app.include_router(api_v1_router)
 
 if __name__ == "__main__":
     uvicorn.run("main:app", reload=True)
