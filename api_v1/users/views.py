e92fcd19ffb2a343cbe22b724fa2184858323575
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-06 17:31:11 +0300

    added a login user view that return access and refresh token upon success

--- api_v1/users/views.py
+++ api_v1/users/views.py
@@ -1,12 +1,27 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, status, Response
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.users.crud import create_user
+from api_v1.users.crud import create_user, login_user
 from api_v1.users.schemas import UserCreate
 from utils.db_helper import db_helper
+from utils.token_helpers import create_access_token, create_refresh_token
+from utils.token_model import TokenModel
 
 router = APIRouter(prefix='/users')
 
 @router.post('/create_user')
 async def register_user_view(user_data: UserCreate, session: AsyncSession = Depends(db_helper.session_getter)):
     return await create_user(user_data=user_data, session=session)
+
+
+@router.get('/login_user', response_model=TokenModel, status_code=status.HTTP_200_OK)
+async def login_user_view(response: Response, username: str, password: str, session: AsyncSession = Depends(db_helper.session_getter)) -> TokenModel:
+    user = await login_user(username=username,
+                            password=password,
+                            session=session)
+    access_token = create_access_token(user=user)
+    refresh_token = create_refresh_token(user=user)
+    response.set_cookie("access_token", access_token, httponly=True)
+    response.set_cookie("refresh_token", refresh_token, httponly=True)
+    return TokenModel(access_token=access_token,
+                      refresh_token=refresh_token)
