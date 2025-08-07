7b6983e2f1c4cdbe4895394a1284b9f42577fd53
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-07 16:55:32 +0300

    added a profiles router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -2,6 +2,10 @@ from fastapi import APIRouter
 
 from .users.views import router as users_router
 
+from .profile.views import router as profiles_router
+
 router = APIRouter(prefix='/api_v1')
 
 router.include_router(users_router)
+
+router.include_router(profiles_router)
