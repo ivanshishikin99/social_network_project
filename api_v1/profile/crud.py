25bf631bbf7ccd85eb6ab05261b7b22c2be52378
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-08 02:23:38 +0300

    added update user partial and update user full operations

--- api_v1/profile/crud.py
+++ api_v1/profile/crud.py
@@ -1,6 +1,7 @@
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
+from api_v1.profile.schemas import ProfileUpdatePartial, ProfileUpdateFull
 from core.models import Profile
 
 
@@ -8,4 +9,21 @@ async def get_profile_by_user_id(user_id: int, session: AsyncSession) -> Profile
     statement = select(Profile).where(Profile.user_id==user_id)
     profile = await session.execute(statement)
     profile = profile.scalar_one()
+    return profile
+
+
+async def update_user_profile_partial(profile: Profile, profile_data: ProfileUpdatePartial, session: AsyncSession) -> Profile:
+    for k, v in profile_data.model_dump().items():
+        if k:
+            setattr(profile, k, v)
+    await session.commit()
+    await session.refresh(profile)
+    return profile
+
+
+async def update_user_profile_full(profile: Profile, profile_data: ProfileUpdateFull, session: AsyncSession) -> Profile:
+    for k, v in profile_data.model_dump().items():
+        setattr(profile, k, v)
+    await session.commit()
+    await session.refresh(profile)
     return profile
