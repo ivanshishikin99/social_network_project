0d293d6133f44043f4dde4c57519db3147934fb4
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-08 01:44:25 +0300

    added a get user profile endpoint

--- api_v1/profile/views.py
+++ api_v1/profile/views.py
@@ -1,3 +1,16 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, status, Depends, Response, Request
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from api_v1.profile.crud import get_profile_by_user_id
+from api_v1.profile.schemas import ProfileRead
+from core.models import Profile, User
+from utils.db_helper import db_helper
+from utils.token_helpers import get_user_by_token
 
 router = APIRouter(prefix="/profile")
+
+
+@router.get('/user_profile', response_model=ProfileRead, status_code=status.HTTP_200_OK)
+async def get_user_profile_view(request: Request, response: Response, session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
+    user = await get_user_by_token(response=response, session=session, request=request)
+    return await get_profile_by_user_id(user_id=user.id, session=session)
