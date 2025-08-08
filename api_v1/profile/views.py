dbf33849eaf952641a96a678c619a14feaef9843
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-08 13:14:14 +0300

    added a Profiles tag added a get_profile_by_id endpoint that separated public and private profiles

--- api_v1/profile/views.py
+++ api_v1/profile/views.py
@@ -1,21 +1,26 @@
+from typing import Union
+
 from fastapi import APIRouter, status, Depends, Response, Request, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.profile.crud import get_profile_by_user_id, update_user_profile_partial, update_user_profile_full
-from api_v1.profile.schemas import ProfileRead, ProfileUpdatePartial, ProfileUpdateFull
+from api_v1.profile.crud import get_profile_by_user_id, update_user_profile_partial, update_user_profile_full, \
+    get_user_by_profile
+from api_v1.profile.dependencies import get_profile_by_profile_id_dependency
+from api_v1.profile.schemas import ProfileReadPublic, ProfileUpdatePartial, ProfileUpdateFull, ProfileReadPrivate
 from core.models import Profile, User
 from utils.db_helper import db_helper
 from utils.token_helpers import get_user_by_token
 
-router = APIRouter(prefix="/profile")
+router = APIRouter(prefix="/profile", tags=["Profiles"])
 
 
-@router.get('/user_profile', response_model=ProfileRead, status_code=status.HTTP_200_OK)
+@router.get('/user_profile', response_model=ProfileReadPublic, status_code=status.HTTP_200_OK)
 async def get_user_profile_view(request: Request, response: Response, session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
     user = await get_user_by_token(response=response, session=session, request=request)
     return await get_profile_by_user_id(user_id=user.id, session=session)
 
-@router.patch("/update_user_profile_partial", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+
+@router.patch("/update_user_profile_partial", response_model=ProfileReadPublic, status_code=status.HTTP_200_OK)
 async def update_user_profile_partial_view(profile_data: ProfileUpdatePartial, request: Request,
                                            response: Response, session: AsyncSession = Depends(db_helper.session_getter)) -> Profile | HTTPException:
     user = await get_user_by_token(response=response, session=session, request=request)
@@ -23,9 +28,18 @@ async def update_user_profile_partial_view(profile_data: ProfileUpdatePartial, r
     return await update_user_profile_partial(profile=profile, profile_data=profile_data, session=session)
 
 
-@router.put("/update_user_profile_full", response_model=ProfileRead, status_code=status.HTTP_200_OK)
+@router.put("/update_user_profile_full", response_model=ProfileReadPublic, status_code=status.HTTP_200_OK)
 async def update_user_profile_full_view(profile_data: ProfileUpdateFull, request: Request,
                                         response: Response, session: AsyncSession = Depends(db_helper.session_getter)) -> Profile:
     user = await get_user_by_token(response=response, session=session, request=request)
     profile = await get_profile_by_user_id(user_id=user.id, session=session)
     return await update_user_profile_full(profile=profile, profile_data=profile_data, session=session)
+
+
+@router.get("/profile", response_model=ProfileReadPublic, status_code=status.HTTP_200_OK)
+async def get_profile_view(profile_id: int, session: AsyncSession = Depends(db_helper.session_getter),
+                           profile: Profile = Depends(get_profile_by_profile_id_dependency)) -> Profile | HTTPException:
+    if profile.is_public:
+        return profile
+    else:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="This profile is private.")
