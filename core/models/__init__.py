8c3beab171b83f28ecd9bfd51a35fc6df9d9283c
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-08 16:06:46 +0300

    created a verification tokens table

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -1,9 +1,11 @@
 __all__ = ("Base",
            "User",
-           "Profile"
+           "Profile",
+           "VerificationToken",
            )
 
 
 from .base import Base
 from .user import User
 from .profile import Profile
+from .verification_token import VerificationToken
---
+++ core/models/verification_token.py
@@ -0,0 +1,13 @@
+import uuid
+
+from sqlalchemy import ForeignKey
+from sqlalchemy.orm import Mapped, mapped_column
+
+from core.models import Base
+
+from core.models.mixins import IdIntPkMixin, CreatedAtMixin
+
+
+class VerificationToken(Base, IdIntPkMixin, CreatedAtMixin):
+    token: Mapped[uuid.UUID]
+    user_email: Mapped[str] = mapped_column(ForeignKey("user.email"))
