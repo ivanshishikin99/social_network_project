cc345998a060cd831942582922a6a9767d47496c
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 16:00:21 +0300

    added a posts router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -4,8 +4,12 @@ from .users.views import router as users_router
 
 from .profile.views import router as profiles_router
 
+from .posts.views import router as posts_router
+
 router = APIRouter(prefix='/api_v1')
 
 router.include_router(users_router)
 
 router.include_router(profiles_router)
+
+router.include_router(posts_router)
