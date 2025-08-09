ab6dae02b6c0d6fea2c5dae5b0f53b1c81bbead8
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 16:23:01 +0300

    added a function for retrieving posts by id

--- api_v1/posts/crud.py
+++ api_v1/posts/crud.py
@@ -1,6 +1,6 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.posts.schemas import PostCreate
+from api_v1.posts.schemas import PostCreate, PostUpdatePartial
 from core.models import Post, User
 
 
@@ -11,3 +11,15 @@ async def create_post(post_data: PostCreate, session: AsyncSession, user: User) 
     await session.commit()
     await session.refresh(post)
     return post
+
+
+async def get_post_by_id(post_id: int, session: AsyncSession) -> Post | None:
+    return await session.get(Post, post_id)
+
+
+# async def update_post_partial(post: Post, post_data: PostUpdatePartial, session: AsyncSession) -> Post:
+#     for k, v in post_data.model_dump().items():
+#         if k:
+#             setattr(post_data, k, v)
+#     await session.commit()
+#     return
