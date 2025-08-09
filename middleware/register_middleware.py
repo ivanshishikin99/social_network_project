94d7f262e2bf9d914b50a46afe4148963810aa04
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 12:15:13 +0300

    added a function that registers middleware into the app

---
+++ middleware/register_middleware.py
@@ -0,0 +1,12 @@
+from fastapi import FastAPI
+from starlette.middleware.base import BaseHTTPMiddleware
+from starlette.middleware.cors import CORSMiddleware
+
+from middleware.logging_middleware import logging_middleware
+
+
+def register_middleware(app: FastAPI):
+    app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"])
+
+    app.add_middleware(BaseHTTPMiddleware, dispatch=logging_middleware)
+
