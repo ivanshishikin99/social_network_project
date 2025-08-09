eea8b82166560d3f5ae7341dcc0aeaf06e2a041e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 16:46:08 +0300

    added an endpoint for fully(put) updating posts

--- api_v1/posts/views.py
+++ api_v1/posts/views.py
@@ -3,9 +3,9 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette.responses import Response
 
-from api_v1.posts.crud import create_post, delete_post, update_post_partial
+from api_v1.posts.crud import create_post, delete_post, update_post_partial, update_post_full
 from api_v1.posts.dependencies import get_post_by_id_dependency
-from api_v1.posts.schemas import PostCreate, PostRead, PostUpdatePartial
+from api_v1.posts.schemas import PostCreate, PostRead, PostUpdatePartial, PostUpdateFull
 from core.models import Post
 from utils.db_helper import db_helper
 from utils.token_helpers import get_user_by_token
@@ -42,4 +42,12 @@ async def update_post_partial_view(request: Request, response: Response, post_id
     user = await get_user_by_token(request=request, response=response, session=session)
     return await update_post_partial(post=post, post_data=post_data, user_id=user.id, session=session)
 
+
+@router.put("/update_post_full", status_code=status.HTTP_200_OK, response_model=PostRead)
+async def update_post_full_view(request: Request, response: Response, post_id: int,
+                                post_data: PostUpdateFull,
+                                session: AsyncSession = Depends(db_helper.session_getter),
+                                post: Post = Depends(get_post_by_id_dependency)) -> Post:
+    user = await get_user_by_token(request=request, response=response, session=session)
+    return await update_post_full(post=post, post_data=post_data, user_id=user.id, session=session)
 
