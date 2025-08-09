e811a79cca17f1e86998cf8d64d8aaf597ac1978
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 16:31:43 +0300

    added an endpoint for deleting posts

--- api_v1/posts/views.py
+++ api_v1/posts/views.py
@@ -3,7 +3,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from starlette.requests import Request
 from starlette.responses import Response
 
-from api_v1.posts.crud import create_post
+from api_v1.posts.crud import create_post, delete_post
 from api_v1.posts.dependencies import get_post_by_id_dependency
 from api_v1.posts.schemas import PostCreate, PostRead
 from core.models import Post
@@ -24,3 +24,12 @@ async def create_post_view(request: Request, response: Response, post_data: Post
 async def get_post_by_id_view(post_id: int, session: AsyncSession = Depends(db_helper.session_getter),
                               post: Post = Depends(get_post_by_id_dependency)) -> Post:
     return post
+
+
+@router.delete("/delete_post", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_post_view(request: Request, response: Response, post_id: int,
+                      session: AsyncSession = Depends(db_helper.session_getter),
+                      post: Post = Depends(get_post_by_id_dependency)):
+    user = await get_user_by_token(request=request, response=response, session=session)
+    return await delete_post(post=post, session=session, user_id=user.id)
+
