a46b93e74774efa627f1f9de0f26c8221883734f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-09 16:23:20 +0300

    added an endpoint for retrieving posts by id

--- api_v1/posts/views.py
+++ api_v1/posts/views.py
@@ -4,6 +4,7 @@ from starlette.requests import Request
 from starlette.responses import Response
 
 from api_v1.posts.crud import create_post
+from api_v1.posts.dependencies import get_post_by_id_dependency
 from api_v1.posts.schemas import PostCreate, PostRead
 from core.models import Post
 from utils.db_helper import db_helper
@@ -17,3 +18,9 @@ async def create_post_view(request: Request, response: Response, post_data: Post
                            session: AsyncSession = Depends(db_helper.session_getter)) -> Post:
     user = await get_user_by_token(request=request, response=response, session=session)
     return await create_post(post_data=post_data, session=session, user=user)
+
+
+@router.get("/get_post_by_id", response_model=PostRead, status_code=status.HTTP_200_OK)
+async def get_post_by_id_view(post_id: int, session: AsyncSession = Depends(db_helper.session_getter),
+                              post: Post = Depends(get_post_by_id_dependency)) -> Post:
+    return post
