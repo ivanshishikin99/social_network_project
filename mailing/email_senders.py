2122e614b11a734b4c5d0d0adf4f010b98b1bd26
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-11 17:18:59 +0300

    verification token now correctly expect a uuid.UUID type instead of str

--- mailing/email_senders.py
+++ mailing/email_senders.py
@@ -25,7 +25,7 @@ def send_welcome_email(username: str, email: str):
                       body=f"Welcome to our site, {username}!")
 
 
-def send_verification_email(user_id: str, user_email: str, verification_token: uuid.UUID):
+def send_verification_email(user_id: int, user_email: str, verification_token: uuid.UUID):
     return send_email(recipient=user_email,
                       subject="Email verification",
                       body=f"Your verification code is {verification_token}. If this e-mail was sent by mistake just ignore it. The code is only valid for 60 minutes.")
