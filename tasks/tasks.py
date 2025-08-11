9c2201cbd44c9a2ed4e2b1757b6bb22262781ef3
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-11 20:30:48 +0300

    added a task for sending password reset tokens

--- tasks/tasks.py
+++ tasks/tasks.py
@@ -5,6 +5,7 @@ from tasks.celery_conf import celery
 
 from mailing.email_senders import send_welcome_email as send_welcome
 from mailing.email_senders import send_verification_email as send_verification
+from mailing.email_senders import send_password_reset_token_email as send_reset
 
 @celery.task(bind=True, max_retries=5)
 def send_welcome_email(self, user_id: int, username: str, email: str):
@@ -20,5 +21,13 @@ def send_verification_email(self, user_id: int, user_email: str, verification_co
     try:
         log.info("Sending verification email to user with id: %s", user_id)
         return send_verification(user_id=user_id, user_email=user_email, verification_token=verification_code)
+    except:
+        self.retry()
+
+@celery.task(bind=True, max_retries=5)
+def send_password_reset_email(self, user_email: str, password_reset_token: uuid.UUID):
+    try:
+        log.info("Sending password reset email %s", user_email)
+        return send_reset(user_email=user_email, password_reset_token=password_reset_token)
     except:
         self.retry()
