e98a0c611500d73dd76bc148869bf7b56b162610
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-11 21:18:45 +0300

    added a posts-comments relationship

--- core/models/post.py
+++ core/models/post.py
@@ -1,13 +1,17 @@
-from typing import List
+from typing import TYPE_CHECKING
 
 from sqlalchemy import ForeignKey
-from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy.orm import Mapped, mapped_column, relationship
 
 from core.models import Base
 from core.models.mixins import IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin
+
+if TYPE_CHECKING:
+    from .comment import Comment
 
 
 class Post(Base, IdIntPkMixin, CreatedAtMixin, UpdatedAtMixin):
     text: Mapped[str] = mapped_column(nullable=False)
     tags: Mapped[str] = mapped_column(nullable=True)
     user_id: Mapped[int] = mapped_column(ForeignKey("user.id"))
+    comments: Mapped[list["Comment"]] = relationship(cascade="all, delete")
