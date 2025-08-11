08d4c3b6728a6f8ed8bc9df87dedb6a431dc9839
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-11 20:31:50 +0300

    added function for getting user by email and verifying password reset tokens

--- api_v1/users/crud.py
+++ api_v1/users/crud.py
@@ -1,3 +1,5 @@
+import uuid
+
 from fastapi import HTTPException, status
 from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
@@ -5,6 +7,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from api_v1.users.schemas import UserCreate
 from core.models import User, Profile
 from utils.password_helpers import hash_password, verify_password, validate_password
+from utils.token_helpers import get_password_reset_token_by_user_email
 
 
 async def create_user(user_data: UserCreate, session: AsyncSession) -> User:
@@ -35,6 +38,13 @@ async def get_user_by_id(user_id: int, session: AsyncSession) -> User | None:
     return await session.get(User, user_id)
 
 
+async def get_user_by_email(user_email: str, session: AsyncSession) -> User | None:
+    statement = select(User).where(User.email == user_email)
+    user = await session.execute(statement)
+    user = user.scalar_one()
+    return user
+
+
 async def delete_user(user: User, session: AsyncSession):
     await session.delete(user)
     await session.commit()
@@ -49,3 +59,15 @@ async def change_password(password: str, new_password: str, user: User, session:
             await session.refresh(user)
             return user
     raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong password")
+
+
+async def verify_password_reset_token(new_password: str, user_email: str, password_reset_token: uuid.UUID,
+                                      session: AsyncSession, user: User):
+    token = await get_password_reset_token_by_user_email(user_email=user_email, session=session)
+    if not password_reset_token == token.token:
+        raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail="Wrong code.")
+    if validate_password(cls=None, val=new_password):
+        user.password = hash_password(password=new_password)
+        await session.commit()
+        await session.refresh(user)
+    return "Your password has been changed!"
