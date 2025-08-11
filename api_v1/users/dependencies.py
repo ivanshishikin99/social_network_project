5d6ddec0c2f30b488b48fd62d687cb7aec175059
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-11 20:32:02 +0300

    added a dependency for getting a user by email

--- api_v1/users/dependencies.py
+++ api_v1/users/dependencies.py
@@ -3,12 +3,20 @@ from sqlalchemy.ext.asyncio import AsyncSession
 
 from core.models import User
 from utils.db_helper import db_helper
-from .crud import get_user_by_id
+from .crud import get_user_by_id, get_user_by_email
 
 
 async def get_user_by_id_dependency(user_id: int, session: AsyncSession = Depends(db_helper.session_getter)) -> User | HTTPException:
     try:
         user = await get_user_by_id(user_id=user_id, session=session)
+    except:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
+    return user
+
+
+async def get_user_by_email_dependency(user_email: str, session: AsyncSession = Depends(db_helper.session_getter)) -> User | HTTPException:
+    try:
+        user = await get_user_by_email(user_email=user_email, session=session)
     except:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="User not found")
     return user
