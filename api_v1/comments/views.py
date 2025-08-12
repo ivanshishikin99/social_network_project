171f222c63bab0a6d9f9e25605065d5f573797f5
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-12 13:40:39 +0300

    post authors and comment authors now can delete comments

--- api_v1/comments/views.py
+++ api_v1/comments/views.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status, Request, Response, Depends
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from api_v1.comments.crud import create_comment
+from api_v1.comments.crud import create_comment, delete_comment
 from api_v1.comments.dependencies import get_comment_by_id_dependency
 from api_v1.comments.schemas import CommentRead, CommentCreate
 from core.models import Comment
@@ -24,3 +24,12 @@ async def create_comment_view(request: Request, response: Response,
 @router.get("/comment/{comment_id}", status_code=status.HTTP_200_OK, response_model=CommentRead)
 async def get_comment_by_id_view(comment_id: int, comment: Comment = Depends(get_comment_by_id_dependency)) -> Comment:
     return comment
+
+
+@router.delete("/comment/{comment_id}", status_code=status.HTTP_204_NO_CONTENT)
+async def delete_comment_by_id_view(request: Request, response: Response,
+                                    comment_id: int, comment: Comment = Depends(get_comment_by_id_dependency),
+                                    session: AsyncSession = Depends(db_helper.session_getter)):
+    user = await get_user_by_token(request=request, response=response, session=session)
+    return await delete_comment(comment=comment, session=session, user_id=user.id)
+
