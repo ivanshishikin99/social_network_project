63ade624126c0dec3f88b3d0633ed4fb9a471eda
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-12 13:40:12 +0300

    added a function for deleting comments

--- api_v1/comments/crud.py
+++ api_v1/comments/crud.py
@@ -2,7 +2,7 @@ from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 
 from api_v1.comments.schemas import CommentCreate
-from core.models import Comment
+from core.models import Comment, Post
 
 
 async def create_comment(user_id: int, post_id: int, comment_data: CommentCreate, session: AsyncSession) -> Comment:
@@ -17,3 +17,13 @@ async def create_comment(user_id: int, post_id: int, comment_data: CommentCreate
 
 async def get_comment_by_id(comment_id: int, session: AsyncSession) -> Comment | None:
     return await session.get(Comment, comment_id)
+
+
+async def delete_comment(user_id: int, comment: Comment, session: AsyncSession):
+    post = await session.get(Post, comment.post_id)
+    print(user_id)
+    print(post.user_id)
+    if user_id == comment.user_id or user_id == post.user_id:
+        await session.delete(comment)
+        await session.commit()
+        return {"Comment deleted successfully!"}
