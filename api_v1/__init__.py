aa6f1ac257baa66ba484698f0c61566489ee5bd5
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-14 13:01:21 +0300

    created a messages router

--- api_v1/__init__.py
+++ api_v1/__init__.py
@@ -8,6 +8,8 @@ from .posts.views import router as posts_router
 
 from .comments.views import router as comments_router
 
+from .messages.views import router as messages_router
+
 router = APIRouter(prefix='/api_v1')
 
 router.include_router(users_router)
@@ -17,3 +19,5 @@ router.include_router(profiles_router)
 router.include_router(posts_router)
 
 router.include_router(comments_router)
+
+router.include_router(messages_router)
