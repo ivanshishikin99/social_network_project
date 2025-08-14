2908359738aff9fdecbf6f2d7341128976ec87a5
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-14 22:09:26 +0300

    fixed the session dependency

--- api_v1/messages/dependencies.py
+++ api_v1/messages/dependencies.py
@@ -6,7 +6,7 @@ from core.models import Message
 from utils.db_helper import db_helper
 
 
-async def get_message_by_id_dependency(message_id: int, session: AsyncSession = Depends(db_helper)) -> Message | HTTPException:
+async def get_message_by_id_dependency(message_id: int, session: AsyncSession = Depends(db_helper.session_getter)) -> Message | HTTPException:
     try:
         message = await get_message_by_id(message_id=message_id, session=session)
     except:
