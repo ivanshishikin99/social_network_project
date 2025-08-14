5c1dd26e1623bb0e24d37166c6440c0549b0c59f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-14 21:51:56 +0300

    added a function for getting messages by id

--- api_v1/messages/crud.py
+++ api_v1/messages/crud.py
@@ -21,3 +21,8 @@ async def get_all_messages_for_user(user_id: int, session: AsyncSession) -> Sequ
     messages = await session.execute(statement)
     messages = messages.scalars().all()
     return messages
+
+
+async def get_message_by_id(message_id: int, session: AsyncSession) -> Message | None:
+    return await session.get(Message, message_id)
+
