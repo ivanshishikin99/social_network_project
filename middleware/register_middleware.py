b1fddaf2be313da93d07349426f201439c431b6f
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-16 12:10:06 +0300

    registered slowapi middleware

--- middleware/register_middleware.py
+++ middleware/register_middleware.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from slowapi.middleware import SlowAPIMiddleware
 from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.middleware.cors import CORSMiddleware
 
@@ -9,4 +10,6 @@ def register_middleware(app: FastAPI):
     app.add_middleware(CORSMiddleware, allow_origins=["*"], allow_headers=["*"], allow_methods=["*"])
 
     app.add_middleware(BaseHTTPMiddleware, dispatch=logging_middleware)
+
+    app.add_middleware(SlowAPIMiddleware)
 
