d59e4a4412ada9e358c4b49c9830db8351515a0e
Ivan Shishkin <ivanshishkin99@bk.ru>
2025-08-16 12:12:33 +0300

    added exception handling for rate limiting

--- exception_handlers.py
+++ exception_handlers.py
@@ -1,11 +1,13 @@
 import fastapi
 from fastapi import FastAPI, Request
 from fastapi.responses import ORJSONResponse
-
-
+from slowapi import _rate_limit_exceeded_handler
+from slowapi.errors import RateLimitExceeded
 
 
 def register_error_handlers(app: FastAPI) -> None:
     @app.exception_handler(fastapi.exceptions.ResponseValidationError)
     async def not_found_exception_handler(request: Request, exc: fastapi.exceptions.ResponseValidationError):
         return ORJSONResponse(status_code=404, content="Item not found!")
+
+    app.add_exception_handler(RateLimitExceeded, _rate_limit_exceeded_handler)
